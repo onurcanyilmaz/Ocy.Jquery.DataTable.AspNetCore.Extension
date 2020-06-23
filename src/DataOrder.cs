9b5af4ea5965bff7e8faed38935ff0d8cb69596c
onur.yilmaz <onur.yilmaz@ONRYILMAZ-MS>
2020-06-23 17:57:41 +0300

    editing a concreate classes

--- src/DataOrder.cs
+++ src/DataOrder.cs
@@ -2,7 +2,7 @@ namespace src
 {
     public class DataOrder : IDataOrder
     {
-        public int Column { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        public string Dir { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public int Column { get; set; }
+        public string Dir { get; set; }
     }
 }
--- src/DtColumn.cs
+++ src/DtColumn.cs
@@ -2,10 +2,10 @@ namespace src
 {
     public class DtColumn : IDtColumn
     {
-        public string Data { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        public string Name { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        public bool Orderable { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        public bool Searchable { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        public DtSearch Search { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string Data { get; set; }
+        public string Name { get; set; }
+        public bool Orderable { get; set; }
+        public bool Searchable { get; set; }
+        public DtSearch Search { get; set; }
     }
 }
